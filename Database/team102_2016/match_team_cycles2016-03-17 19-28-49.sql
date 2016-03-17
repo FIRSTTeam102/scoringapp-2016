697a0e7fc4d188b18b8f12fc22b3fee087d88786
Mike Parker <mike.parker@linde.com>
2016-03-17 19:29:22 -0400

    Export of remote database.

---
+++ Database/team102_2016/match_team_cycles2016-03-17 19-28-49.sql
@@ -0,0 +1,14 @@
+USE team102_2016;
+
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,56,1,'CDF','Y');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,56,2,'CDF','N');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,56,3,'CDF','Y');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,56,4,'CDF','Y');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,56,5,'CDF','Y');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,56,8,'LOW','Y');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,56,10,'LOW','N');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,219,2,'CDF','N');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,219,6,'CDF','Y');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,219,7,'CDF','Y');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,219,9,'HGH','Y');
+insert into `match_team_cycles`(`tournament_id`,`match_number`,`team_number`,`cycle_number`,`operation_attempted`,`success`) values ('MTO',1,219,11,'HGH','N');
